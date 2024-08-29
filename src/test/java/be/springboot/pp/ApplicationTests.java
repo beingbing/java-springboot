d0872f1146af72a59e61b71d329b843878c0f408
Samar Shaikh <samar.shaikh@bharatpe.com>
2024-08-29 19:56:23 +0530

    initial setup

---
+++ src/test/java/be/springboot/pp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.springboot.pp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
