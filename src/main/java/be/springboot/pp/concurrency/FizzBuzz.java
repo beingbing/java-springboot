edd2908232f6d73853b5aa2629059f99a83d6b3c
Samar Shaikh <samar.shaikh@bharatpe.com>
2024-09-17 19:26:22 +0530

    fizzbuzz solution

--- src/main/java/be/springboot/pp/concurrency/FizzBuzz.java
+++ src/main/java/be/springboot/pp/concurrency/FizzBuzz.java
@@ -19,79 +19,155 @@ class FizzBuzzMaker {
 
     public static void main(String[] args) {
         System.out.println("FizzBuzzMaker: main: args: " + args + " " + Thread.currentThread().getName());
-        Thread fizz = new Thread(new Fizz());
-        Thread buzz = new Thread(new Buzz());
-        Thread fizzbuzz = new Thread(new FizzBuzz());
-        Thread plain = new Thread(new Plain());
+        Object lock = new Object();
+        Thread fizz = new Thread(new Fizz(lock));
+        Thread buzz = new Thread(new Buzz(lock));
+        Thread fizzbuzz = new Thread(new FizzBuzz(lock));
+        Thread plain = new Thread(new Plain(lock));
         fizz.start();
         buzz.start();
         fizzbuzz.start();
         plain.start();
-
         System.out.println("FizzBuzzMaker: main: ends " + Thread.currentThread().getName());
     }
 }
 
 // 3, !5
 class Fizz implements Runnable {
+
+    private final Object lock;
+
+    public Fizz(Object lock) {
+        this.lock = lock;
+    }
+
     @Override
     public void run() {
         System.out.println("Fizz: run: " + Thread.currentThread().getName());
-        while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
-            while (!(FizzBuzzMaker.i % 3 == 0 && FizzBuzzMaker.i % 5 != 0)) {
-//                System.out.println("busy waiting inside fz: " + Thread.currentThread().getName());
+        synchronized(lock) {
+            while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                while (!(FizzBuzzMaker.i % 3 == 0 && FizzBuzzMaker.i % 5 != 0) && FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                    System.out.println("busy waiting inside fz: " + Thread.currentThread().getName());
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("fz ex: " + e.getMessage());
+                    }
+                }
+                if (FizzBuzzMaker.i > FizzBuzzMaker.n) {
+                    lock.notifyAll();
+                    break;
+                }
+                System.out.println(FizzBuzzMaker.i + " Fizz");
+                FizzBuzzMaker.i++;
+                lock.notifyAll();
             }
-            System.out.println(FizzBuzzMaker.i + " Fizz");
-            FizzBuzzMaker.i++;
+            System.out.println("Fizz: run: ends " + Thread.currentThread().getName());
         }
-        System.out.println("Fizz: run: ends " + Thread.currentThread().getName());
     }
 }
 
 // !3, 5
 class Buzz implements Runnable {
+
+    private final Object lock;
+
+    public Buzz(Object lock) {
+        this.lock = lock;
+    }
+
     @Override
     public void run() {
         System.out.println("Buzz: run: " + Thread.currentThread().getName());
-        while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
-            while (!(FizzBuzzMaker.i % 3 != 0 && FizzBuzzMaker.i % 5 == 0)) {
-//                System.out.println("busy waiting inside bz: " + Thread.currentThread().getName());
+        synchronized(lock) {
+            while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                while (!(FizzBuzzMaker.i % 3 != 0 && FizzBuzzMaker.i % 5 == 0) && FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                    System.out.println("busy waiting inside bz: " + Thread.currentThread().getName());
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("bz ex: " + e.getMessage());
+                    }
+                }
+                if (FizzBuzzMaker.i > FizzBuzzMaker.n) {
+                    lock.notifyAll();
+                    break;
+                }
+                System.out.println(FizzBuzzMaker.i + " Buzz");
+                FizzBuzzMaker.i++;
+                lock.notifyAll();
             }
-            System.out.println(FizzBuzzMaker.i + " Buzz");
-            FizzBuzzMaker.i++;
+            System.out.println("Buzz: run: ends " + Thread.currentThread().getName());
         }
-        System.out.println("Buzz: run: ends " + Thread.currentThread().getName());
     }
 }
 
 // 3, 5
 public class FizzBuzz implements Runnable {
+
+    private final Object lock;
+
+    public FizzBuzz(Object lock) {
+        this.lock = lock;
+    }
+
     @Override
     public void run() {
         System.out.println("FizzBuzz: run: " + Thread.currentThread().getName());
-        while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
-            while (!(FizzBuzzMaker.i % 3 == 0 && FizzBuzzMaker.i % 5 == 0)) {
-//                System.out.println("busy waiting inside fb: " + Thread.currentThread().getName());
+        synchronized(lock) {
+            while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                while (!(FizzBuzzMaker.i % 3 == 0 && FizzBuzzMaker.i % 5 == 0) && FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                    System.out.println("busy waiting inside fb: " + Thread.currentThread().getName());
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("fb ex: " + e.getMessage());
+                    }
+                }
+                if (FizzBuzzMaker.i > FizzBuzzMaker.n) {
+                    lock.notifyAll();
+                    break;
+                }
+                System.out.println(FizzBuzzMaker.i + " FizzBuzz");
+                FizzBuzzMaker.i++;
+                lock.notifyAll();
             }
-            System.out.println(FizzBuzzMaker.i + " FizzBuzz");
-            FizzBuzzMaker.i++;
+            System.out.println("FizzBuzz: run: ends " + Thread.currentThread().getName());
         }
-        System.out.println("FizzBuzz: run: ends " + Thread.currentThread().getName());
     }
 }
 
 // !3, !5
 class Plain implements Runnable {
+
+    private final Object lock;
+
+    public Plain(Object lock) {
+        this.lock = lock;
+    }
+
     @Override
     public void run() {
         System.out.println("Plain: run: " + Thread.currentThread().getName());
-        while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
-            while (!(FizzBuzzMaker.i % 3 != 0 && FizzBuzzMaker.i % 5 != 0)) {
-//                System.out.println("busy waiting inside pl: " + Thread.currentThread().getName());
+        synchronized(lock) {
+            while (FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                while (!(FizzBuzzMaker.i % 3 != 0 && FizzBuzzMaker.i % 5 != 0) && FizzBuzzMaker.i <= FizzBuzzMaker.n) {
+                    System.out.println("busy waiting inside pl: " + Thread.currentThread().getName());
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("pl ex: " + e.getMessage());
+                    }
+                }
+                if (FizzBuzzMaker.i > FizzBuzzMaker.n) {
+                    lock.notifyAll();
+                    break;
+                }
+                System.out.println(FizzBuzzMaker.i + " Plain");
+                FizzBuzzMaker.i++;
+                lock.notifyAll();
             }
-            System.out.println(FizzBuzzMaker.i + " Plain");
-            FizzBuzzMaker.i++;
+            System.out.println("Plain: run: ends " + Thread.currentThread().getName());
         }
-        System.out.println("Plain: run: ends " + Thread.currentThread().getName());
     }
 }
