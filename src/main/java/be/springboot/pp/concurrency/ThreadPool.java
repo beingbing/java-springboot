84b35542436d034d66b7ad77a0f32c08202e8dfa
Samar Shaikh <samar.shaikh@bharatpe.com>
2024-09-18 11:31:14 +0530

    implement book keeper to replace vacancy created by dead threads

--- src/main/java/be/springboot/pp/concurrency/ThreadPool.java
+++ src/main/java/be/springboot/pp/concurrency/ThreadPool.java
@@ -2,6 +2,7 @@ package be.springboot.pp.concurrency;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -19,11 +20,15 @@ class Simulator {
         Map<Integer, FutureTask<Integer>> intFutureTaskMap = new HashMap<>();
         Random random = new Random();
         int k = 0;
-        for (int i = 0; i < 20; i++) {
-//            System.out.println("Simulator: main: i: " + i);
+        for (int i = 0; i < 21; i++) {
+            System.out.println("Simulator: main: i: " + i);
             FutureTask<Integer> res = null;
-            if (i % 2 == 0) {
-//                System.out.println("Simulator: main: i is even: " + i);
+            if (i % 3 == 0) {
+                System.out.println("Simulator: main: i is divisible by 3: " + i);
+                res = threadPool.submit(new DummyCallback(k++));
+                intFutureTaskMap.put(i, res);
+            } else if (i % 3 == 1) {
+                System.out.println("Simulator: main: i gives 1 when divided by 3: " + i);
                 int qty = 30000;
                 List<Integer> nums = new ArrayList<>();
                 for (int j = 0; j < qty; j++)
@@ -32,18 +37,19 @@ class Simulator {
                 res = threadPool.submit(new PairCounter(nums, sum));
                 intFutureTaskMap.put(i, res);
             } else {
-//                System.out.println("Simulator: main: i is odd: " + i);
-                res = threadPool.submit(new DummyCallback(k++));
-                intFutureTaskMap.put(i, res);
+                System.out.println("Simulator: main: i gives 2 when divided by 3: " + i);
+                threadPool.submit(new ThreadKill());
             }
         }
         threadPool.shutdown();
 
         for (int i = 0; i < 20; i++) {
-            System.out.println("Simulator: main: intFutureTaskMap: Req #" + i + ": " + intFutureTaskMap.get(i).get());
+            System.out.println(" ++++++ Simulator: Req #" + i + ": response: " + intFutureTaskMap.get(i).get());
         }
     }
 }
+
+
 
 public class ThreadPool {
 
@@ -53,9 +59,9 @@ public class ThreadPool {
 
     private final BlockingQueue<Runnable> taskQueue;
 
-//    private final Set<Integer> deadThreadIds;
+    private final Set<Integer> deadThreadIdList;
 
-//    private final Thread bookKeeper;
+    private final Thread bookKeeper;
 
     private boolean isShutDownInitiated;
 
@@ -65,12 +71,15 @@ public class ThreadPool {
         this.poolCapacity = numThreads;
         this.taskQueue = new ArrayBlockingQueue<>(tasksPerThread);
         this.threads = new ArrayList<>();
+        this.deadThreadIdList = new HashSet<>();
         for (int i = 0; i < numThreads; i++) {
-            Thread thread = new Thread(new WorkerThread(i, taskQueue));
+            Thread thread = new Thread(new WorkerThread(i, taskQueue, deadThreadIdList));
             threads.add(thread);
         }
         for (Thread thread : threads) thread.start();
         this.isShutDownInitiated = false;
+        this.bookKeeper = new Thread(new BookKeeper(deadThreadIdList, threads, taskQueue));
+        this.bookKeeper.start();
     }
 
     public synchronized FutureTask<Integer> submit(Callable<Integer> task) throws InterruptedException {
@@ -78,6 +87,11 @@ public class ThreadPool {
         FutureTask<Integer> futureTask = new FutureTask<>(task);
         this.taskQueue.put(futureTask);
         return futureTask;
+    }
+
+    public synchronized void submit(Runnable task) throws InterruptedException {
+        if (this.isShutDownInitiated) throw new InterruptedException("Thread pool is shut down");
+        this.taskQueue.put(task);
     }
 
     public synchronized void shutdown() throws InterruptedException {
@@ -88,15 +102,20 @@ public class ThreadPool {
 
 }
 
+
+
 class WorkerThread implements Runnable {
 
     private final int id;
 
     private final BlockingQueue<Runnable> taskQueue;
 
-    public WorkerThread(int id, BlockingQueue<Runnable> taskQueue) {
+    private final Set<Integer> deadThreadIdList;
+
+    public WorkerThread(int id, BlockingQueue<Runnable> taskQueue, Set<Integer> deadThreadIdList) {
         this.id = id;
         this.taskQueue = taskQueue;
+        this.deadThreadIdList = deadThreadIdList;
     }
 
     @Override
@@ -115,11 +134,19 @@ class WorkerThread implements Runnable {
                     System.out.println("WorkerThread: run: ends: " + id);
                     break;
                 }
+                synchronized(deadThreadIdList) {
+                    deadThreadIdList.add(id);
+                    deadThreadIdList.notifyAll();
+                }
+                System.out.println("WorkerThread: id#" + id + " is dead");
+                throw new RuntimeException(e.getMessage());
             }
         }
-        System.out.println("WorkerThread: id: " + id + " ends");
+        System.out.println("WorkerThread: id#" + id + " exiting ...");
     }
 }
+
+
 
 class DummyCallback implements Callable<Integer> {
 
@@ -136,9 +163,64 @@ class DummyCallback implements Callable<Integer> {
     }
 }
 
+
+
 class ShutterDown implements Runnable {
     @Override
     public void run() {
         throw new RuntimeException("Thread pool is shut down");
+    }
+}
+
+
+
+class ThreadKill implements Runnable {
+    @Override
+    public void run() {
+        throw new RuntimeException("Thread killed ;)");
+    }
+}
+
+
+
+class BookKeeper implements Runnable {
+
+    private final Set<Integer> deadThreadIdList;
+
+    private final List<Thread> threads;
+
+    private final BlockingQueue<Runnable> taskQueue;
+
+    public BookKeeper(Set<Integer> deadThreadIdList, List<Thread> threads, BlockingQueue<Runnable> taskQueue) {
+        this.deadThreadIdList = deadThreadIdList;
+        this.threads = threads;
+        this.taskQueue = taskQueue;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized(deadThreadIdList) {
+                while (deadThreadIdList.isEmpty()) {
+                    try {
+                        deadThreadIdList.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                for (var i : deadThreadIdList) {
+                    Thread t = new Thread(new WorkerThread(i, taskQueue, deadThreadIdList));
+                    threads.set(i, t);
+                    t.start();
+                    deadThreadIdList.remove(i);
+                    System.out.println("BookKeeper: new Thread at id: " + i + " is: " + t);
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
