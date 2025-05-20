3f3bb98cd396df11cc624ab597b652b2fe5f897c
Samar Shaikh <samar.shaikh@bharatpe.com>
2025-05-20 05:31:22 +0530

    Food-delivery system files location updated.

---
+++ src/main/java/be/springboot/pp/fooddeliverysystem/service/searchers/FoodItemSearcher.java
@@ -0,0 +1,31 @@
+package be.springboot.pp.fooddeliverysystem.service.searchers;
+
+import be.springboot.pp.fooddeliverysystem.dtos.FoodItem;
+import be.springboot.pp.fooddeliverysystem.pojos.DataAccessResult;
+import be.springboot.pp.fooddeliverysystem.pojos.DataAccessObjectConverter;
+import be.springboot.pp.fooddeliverysystem.service.filters.FoodItemFilter;
+import be.springboot.pp.fooddeliverysystem.repository.DataAccessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodItemSearcher {
+
+    public List<FoodItem> search(String foodItemName, List<FoodItemFilter> filters) {
+        if (foodItemName.isBlank() || foodItemName.isEmpty() || filters.isEmpty()) throw new IllegalArgumentException("missing params");
+
+        DataAccessResult result = DataAccessor.getFoodItemsByName(foodItemName);
+        List<FoodItem> foodItems = DataAccessObjectConverter.convertToFoodItems(result);
+        for (FoodItemFilter filter : filters) {
+            List<FoodItem> filteredFoodItems = new ArrayList<>();
+            for (FoodItem item : foodItems) if (filter.filter(item)) filteredFoodItems.add(item);
+            foodItems = filteredFoodItems;
+        }
+
+        return foodItems;
+    }
+
+    public FoodItem searchById(Long id) {
+        return null;
+    }
+}
