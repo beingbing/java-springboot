92e5c13ddc762ee54e8c2d2745b1824cc0340393
Samar Shaikh <samar.shaikh@bharatpe.com>
2025-05-28 02:33:06 +0530

    challenge 2 completed.

--- src/main/java/be/springboot/pp/concurrency/Challenge2.java
+++ src/main/java/be/springboot/pp/concurrency/Challenge2.java
@@ -10,22 +10,21 @@ package be.springboot.pp.concurrency;
 
 public class Challenge2 {
 
-    public static int cur = 0;
+    public static int cur = 0, rounds = 3;
 
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName() + " started");
         Object lock = new Object();
-        int rounds = 3;
-        Thread t0 = new Thread(new Challenge2Worker(0, lock, rounds));
-        Thread t1 = new Thread(new Challenge2Worker(1, lock, rounds));
-        Thread t2 = new Thread(new Challenge2Worker(2, lock, rounds));
-        Thread t3 = new Thread(new Challenge2Worker(3, lock, rounds));
-        Thread t4 = new Thread(new Challenge2Worker(4, lock, rounds));
-        Thread t5 = new Thread(new Challenge2Worker(5, lock, rounds));
-        Thread t6 = new Thread(new Challenge2Worker(6, lock, rounds));
-        Thread t7 = new Thread(new Challenge2Worker(7, lock, rounds));
-        Thread t8 = new Thread(new Challenge2Worker(8, lock, rounds));
-        Thread t9 = new Thread(new Challenge2Worker(9, lock, rounds));
+        Thread t0 = new Thread(new Challenge2Worker(0, lock));
+        Thread t1 = new Thread(new Challenge2Worker(1, lock));
+        Thread t2 = new Thread(new Challenge2Worker(2, lock));
+        Thread t3 = new Thread(new Challenge2Worker(3, lock));
+        Thread t4 = new Thread(new Challenge2Worker(4, lock));
+        Thread t5 = new Thread(new Challenge2Worker(5, lock));
+        Thread t6 = new Thread(new Challenge2Worker(6, lock));
+        Thread t7 = new Thread(new Challenge2Worker(7, lock));
+        Thread t8 = new Thread(new Challenge2Worker(8, lock));
+        Thread t9 = new Thread(new Challenge2Worker(9, lock));
         t0.start();
         t1.start();
         t2.start();
@@ -43,31 +42,62 @@ public class Challenge2 {
 class Challenge2Worker implements Runnable {
     private final int val;
     private final Object lock;
-    private int rounds;
 
-    public Challenge2Worker(int val, Object lock, int rounds) {
+    public Challenge2Worker(int val, Object lock) {
         this.val = val;
         this.lock = lock;
-        this.rounds = rounds;
     }
+
+//    @Override
+//    public void run() {
+////        while (Challenge2.rounds > 0) { // this will introduce memory visibility issues
+//        while (roundsOver()) { // Thread could have crossed it, and waited in compare() but meanwhile rounds became 0
+//            while (compare()) {}
+//            System.out.println(Thread.currentThread().getName() + " " + val);
+//            synchronized(lock) {
+//                Challenge2.cur = (Challenge2.cur + 1) % 10;
+//                if (Challenge2.cur == 0)
+//                    Challenge2.rounds--;
+//            }
+//        }
+//    }
+// This still won't work because roundsOver() is thread safe, decrementing rounds variable is happening
+// under thread safe environment. But in between ther are still two lines of command which are happening
+// in unsafe environment. Context switch can happen in `while (compare()) {}` and it might happen that
+// when thread crossed roundsOver() checked and entered while, the condition was correct, but while waiting
+// in while (compare()) {} the condition because incorrect. So, we need to secure that compound action with
+// a lock -
 
     @Override
     public void run() {
-        while (rounds > 0) {
+        while (true) {
             while (compare()) {}
-            System.out.println(Thread.currentThread().getName() + " " + val);
             synchronized(lock) {
+                if (Challenge2.rounds == 0) break;
+                System.out.println(Thread.currentThread().getName() + " " + val);
                 Challenge2.cur = (Challenge2.cur + 1) % 10;
+                if (Challenge2.cur == 0)
+                    Challenge2.rounds--;
             }
-            rounds--;
         }
+    }
+
+    private boolean roundsOver() {
+        boolean ans = false;
+        synchronized(lock) {
+            ans = Challenge2.rounds > 0;
+        }
+        return ans;
     }
 
     private boolean compare() {
         boolean ans = false;
         synchronized(lock) {
-            ans = !(val == Challenge2.cur);
-        }
+            ans = !(val == Challenge2.cur)
+                    && Challenge2.rounds > 0; // need this to inform all busy-waiting threads
+        }                                      // that it is time for exiting.
         return ans;
     }
 }
+
+// Graceful termination of all the threads is important.
