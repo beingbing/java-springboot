4c9f8ab07e9894e6c62527e0a5f159e6a5d4fa4f
Samar Shaikh <samar.shaikh@bharatpe.com>
2025-05-28 02:37:01 +0530

    attempting challenge 3

---
+++ src/main/java/be/springboot/pp/concurrency/Challenge3.java
@@ -0,0 +1,9 @@
+package be.springboot.pp.concurrency;
+
+/*
+* You have 2 shared variables, cur (starting from 1) and limit, one thread should print all even
+* number and other all odd number in given range.
+* */
+
+public class Challenge3 {
+}
