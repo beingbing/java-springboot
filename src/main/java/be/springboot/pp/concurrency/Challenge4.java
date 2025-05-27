2978b4252d4a8e71883f6b3b05b41e4f5513b59e
Samar Shaikh <samar.shaikh@bharatpe.com>
2025-05-28 03:12:05 +0530

    challenge 4 completed

---
+++ src/main/java/be/springboot/pp/concurrency/Challenge4.java
@@ -0,0 +1,26 @@
+package be.springboot.pp.concurrency;
+
+/*
+* You have 2 shared variables, cur (starting from 1) and limit, some threads should print all even
+* numbers and other remaining threads all odd number in given range.
+* Each number should be printed only once, but we have a choice for who wants to print it.
+* It should not happen that a thread is printing both even and odd numbers.
+* */
+
+public class Challenge4 {
+    public static final int limit = 20;
+    public static int cur = 1; // we can remove volatile from here now, as we acquire lock due to 2nd issue.
+
+    public static void main(String[] args) {
+        Object lock = new Object();
+        Thread t1 = new Thread(new EvenWorker("first even", lock));
+        Thread t3 = new Thread(new EvenWorker("second even", lock));
+        Thread t2 = new Thread(new OddWorker("first Odd", lock));
+        Thread t4 = new Thread(new OddWorker("second Odd", lock));
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
+// nothing need to change, it will work correctly
