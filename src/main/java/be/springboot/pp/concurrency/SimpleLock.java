b3ac7915a601ba171b654289fec581a473b4c1a2
Samar Shaikh <samar.shaikh@bharatpe.com>
2025-05-28 18:41:33 +0530

    unlocking is done only by the locking thread, also no unlocking if no locking was done

--- src/main/java/be/springboot/pp/concurrency/SimpleLock.java
+++ src/main/java/be/springboot/pp/concurrency/SimpleLock.java
@@ -7,9 +7,11 @@ package be.springboot.pp.concurrency;
 
 public class SimpleLock {
     private boolean isLocked;
+    private Thread lockedBy;
 
     public SimpleLock() {
         this.isLocked = false;
+        this.lockedBy = null;
     }
 
     public synchronized void lock() throws InterruptedException {
@@ -18,14 +20,32 @@ public class SimpleLock {
             wait();
         }
         isLocked = true;
+        lockedBy = Thread.currentThread();
         System.out.println(Thread.currentThread().getId() + " acquired the lock");
     }
 
-    // unlock can be acquired on same object by any other thread as well to release the lock. We need to fix this issue.
+    // unlock can be acquired on same object by any other thread as well to release the lock. We need to fix this issue.✅
     public synchronized void unlock() {
+        if (!isLocked) return; // if no lock is acquired then no unlocking is allowed
+        if (!Thread.currentThread().equals(lockedBy)) return; // malicious thread trying to unlocking w/o locking
         isLocked = false;
+        lockedBy = null;
         notifyAll();
         System.out.println(Thread.currentThread().getId() + " release the lock");
+    }
+}
+
+// w/o acquiring any lock, it will be able to release a lock on simpleLock object acquired by anyone.
+class MaliciousWorker implements Runnable {
+    @Override
+    public void run() {
+     lock.unlock();
+    }
+
+    private final SimpleLock lock;
+
+    MaliciousWorker(SimpleLock lock) {
+        this.lock = lock;
     }
 }
 
@@ -49,13 +69,22 @@ class SimpleWorker implements Runnable {
 }
 
 class SimpleExecutor {
+//    public static void main(String[] args) {
+//        SimpleLock lock = new SimpleLock();
+//        Thread t1 = new Thread(new SimpleWorker(lock));
+//        Thread t2 = new Thread(new SimpleWorker(lock));
+//        Thread t3 = new Thread(new SimpleWorker(lock));
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//    }
+
+    // t2 is releasing lock which was acquired by t1
     public static void main(String[] args) {
         SimpleLock lock = new SimpleLock();
         Thread t1 = new Thread(new SimpleWorker(lock));
-        Thread t2 = new Thread(new SimpleWorker(lock));
-        Thread t3 = new Thread(new SimpleWorker(lock));
+        Thread t2 = new Thread(new MaliciousWorker(lock));
         t1.start();
         t2.start();
-        t3.start();
     }
 }
