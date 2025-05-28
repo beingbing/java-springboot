1b030759503d44b312a0f7b93f6dc7aed7f8ed4f
Samar Shaikh <samar.shaikh@bharatpe.com>
2025-05-28 18:55:30 +0530

    designing reentracy in simple lock

--- src/main/java/be/springboot/pp/concurrency/SimpleLock.java
+++ src/main/java/be/springboot/pp/concurrency/SimpleLock.java
@@ -1,26 +1,29 @@
 package be.springboot.pp.concurrency;
 
 /*
-* synchronized keyword make lock to be acquired on current object hence both lock()
-* and unlock() will be synchronized against the same object.
-* */
+ * synchronized keyword make lock to be acquired on current object hence both lock()
+ * and unlock() will be synchronized against the same object.
+ * */
 
 public class SimpleLock {
     private boolean isLocked;
     private Thread lockedBy;
+    private int count;
 
     public SimpleLock() {
         this.isLocked = false;
         this.lockedBy = null;
+        this.count = 0;
     }
 
     public synchronized void lock() throws InterruptedException {
-        while (isLocked) {
+        while (isLocked && !Thread.currentThread().equals(lockedBy)) {
             System.out.println(Thread.currentThread().getId() + " needs to wait");
             wait();
         }
         isLocked = true;
         lockedBy = Thread.currentThread();
+        count++;
         System.out.println(Thread.currentThread().getId() + " acquired the lock");
     }
 
@@ -28,43 +31,74 @@ public class SimpleLock {
     public synchronized void unlock() {
         if (!isLocked) return; // if no lock is acquired then no unlocking is allowed
         if (!Thread.currentThread().equals(lockedBy)) return; // malicious thread trying to unlocking w/o locking
-        isLocked = false;
-        lockedBy = null;
-        notifyAll();
+        count--;
+        if (count == 0) {
+            isLocked = false;
+            lockedBy = null;
+            notifyAll();
+        }
         System.out.println(Thread.currentThread().getId() + " release the lock");
     }
 }
 
 // w/o acquiring any lock, it will be able to release a lock on simpleLock object acquired by anyone.
 class MaliciousWorker implements Runnable {
-    @Override
-    public void run() {
-     lock.unlock();
-    }
-
     private final SimpleLock lock;
 
     MaliciousWorker(SimpleLock lock) {
         this.lock = lock;
     }
+
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+        } catch (Exception e) {
+            //
+        } finally {
+            lock.unlock();
+        }
+    }
 }
 
 class SimpleWorker implements Runnable {
+    private final SimpleLock lock;
+
+    SimpleWorker(SimpleLock lock) {
+        this.lock = lock;
+    }
+
     @Override
     public void run() {
         try {
             lock.lock();
             Thread.sleep(1000);
+            start();
             lock.unlock();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private final SimpleLock lock;
+    public void start() {
+        try {
+            lock.lock();
+            end();
+        } catch (Exception e) {
+            System.out.println("from start: " + e.getMessage());
+        } finally {
+            lock.unlock();
+        }
+    }
 
-    SimpleWorker(SimpleLock lock) {
-        this.lock = lock;
+    public void end() {
+        try {
+            lock.lock();
+        } catch (Exception e) {
+            System.out.println("from end: " + e.getMessage());
+        } finally {
+            lock.unlock();
+        }
     }
 }
 
