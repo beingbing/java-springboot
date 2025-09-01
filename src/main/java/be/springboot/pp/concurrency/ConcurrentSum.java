2ea8d38b1d3721236ff04fc8c4a92321e43d4856
samar <shaikhsamartaj@gmail.com>
2025-09-01 08:46:01 +0530

    revisited concurrency basics

--- src/main/java/be/springboot/pp/concurrency/ConcurrentSum.java
+++ src/main/java/be/springboot/pp/concurrency/ConcurrentSum.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 // 2
 /*
@@ -18,14 +17,11 @@ public class ConcurrentSum {
     // this is because of contention in accessing sum variable.
     public static AtomicLong sum = new AtomicLong(0);
 
-    public static AtomicInteger cnt = new AtomicInteger(0);
-
     public static void main(String[] args) {
         System.out.println("ConcurrentSum: main: args: " + Arrays.toString(args) + " " + Thread.currentThread().getName());
 
         try {
             List<Integer> nums = new ArrayList<>();
-
             Random random = new Random();
 
             for (int i = 0; i < 1_00_00_000; i++)
@@ -37,7 +33,7 @@ public class ConcurrentSum {
             System.out.println("ConcurrentSum: main: seqSum: time: " + (end - start) + " " + Thread.currentThread().getName());
 
             long start2 = System.currentTimeMillis();
-            concurrentSum(nums);
+            concurrentSum(nums, 4);
             long end2 = System.currentTimeMillis();
             System.out.println("ConcurrentSum: main: concurrentSum: time: " + (end2 - start2) + " " + Thread.currentThread().getName());
         } catch (Exception e) {
@@ -55,28 +51,26 @@ public class ConcurrentSum {
         System.out.println("ConcurrentSum: seqSum: ans: " + ans + " " + Thread.currentThread().getName());
     }
 
-    private static void concurrentSum(List<Integer> nums) throws  InterruptedException {
-        int size = nums.size()/4;
+    private static void concurrentSum(List<Integer> nums, int numThreads) throws InterruptedException {
+        int size = nums.size() / numThreads;
+        Thread[] workers = new Thread[numThreads];
 
-        Thread t1 = new Thread(new Worker(nums, 0, size-1));
-        Thread t2 = new Thread(new Worker(nums, size, size*2-1));
-        Thread t3 = new Thread(new Worker(nums, size*2, size*3-1));
-        Thread t4 = new Thread(new Worker(nums, size*3, size*4-1));
+        sum.set(0); // reset sum before concurrent run
 
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
+        for (int i = 0; i < numThreads; i++) {
+            int left = i * size;
+            int right = (i == numThreads - 1) ? nums.size() - 1 : (left + size - 1);
+            workers[i] = new Thread(new Worker(nums, left, right));
+            workers[i].start();
+        }
 
-        while (cnt.get() < 4) {} // busy waiting
+        for (Thread worker : workers) worker.join();
         System.out.println("ConcurrentSum: concurrentSum: sum: " + sum.get() + " " + Thread.currentThread().getName());
     }
 }
 
 class Worker implements Runnable {
-
     private final List<Integer> nums;
-
     private final int l, r;
 
     public Worker(List<Integer> nums, int l, int r) {
@@ -87,17 +81,13 @@ class Worker implements Runnable {
 
     @Override
     public void run() {
-//        System.out.println("Worker: run: l: " + l + " r: " + r + " " + Thread.currentThread().getName());
-        long s = 0;
+        long localSum = 0;
         for (int i = l; i <= r; i++) {
-            s += nums.get(i);
-//            ConcurrentSum.sum.addAndGet(nums.get(i)); // if we do this instead of above line then
-            // concurrent sum will have worse performance as compared to sequential sum because of contention.
+            localSum += nums.get(i);
+            // ⚠ If we instead did:
+            // ConcurrentSum.sum.addAndGet(nums.get(i));
+            // we'd create massive contention on AtomicLong and performance would tank.
         }
-//        System.out.println("Worker: run: s: " + s + " " + Thread.currentThread().getName());
-        ConcurrentSum.sum.addAndGet(s);
-//        System.out.println("Worker: run: sum: " + sum + " " + Thread.currentThread().getName());
-        ConcurrentSum.cnt.incrementAndGet();
-//        System.out.println("Worker: run: cnt: " + cnt + " " + Thread.currentThread().getName());
+        ConcurrentSum.sum.addAndGet(localSum);
     }
 }
